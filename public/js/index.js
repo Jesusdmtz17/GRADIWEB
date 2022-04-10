b74f592ad33e2189cbbc6ec32b6ddaa9fb524255
Jesusdmtz17 <41216061+Jesusdmtz17@users.noreply.github.com>
2022-04-10 13:15:36 -0500

    writing product options

--- public/js/index.js
+++ public/js/index.js
@@ -24,8 +24,33 @@ window.GradiWebApp = window.GradiWebApp || {
             document.querySelector(_this.selectors.product_name).innerHTML = response_json.title;
             document.querySelector(_this.selectors.product_vendor).innerHTML = response_json.vendor;
             document.querySelector(_this.selectors.product_description).innerHTML = response_json.description;
-            
+            _this.writeOptions(response_json);
+            window.product_variants = response_json.variants;
         })();
+    },
+    writeOptions: function(product){
+        let options = ``;
+        product.options.forEach(element => {
+            element.values.forEach((option, index)=>{
+                if(index == 0){
+                    options += `<div class="option-wrapper">
+                    <label>${element.name}</label>
+                    <div class="option-wrapper-values">`;
+                }
+                options += `<div class="option-${element.name}">
+                    <input name="option-${element.name}" value="${option}" type="radio" class="option-selector" ${index == 0 ? 'checked': ''}>
+                    <label class="option-selector-label">${option}</label>
+                </div>`;
+                
+                if(index == (element.values.length - 1)){
+                    options += `</div>`;
+                }
+            })
+        });
+        document.querySelector(this.selectors.product_variants).innerHTML = options;
+    },
+    calcPrice: function(){
+
     },
     init: function(){
         //master function
